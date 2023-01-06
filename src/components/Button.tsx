ee7366f60286a4ca0e3ec17dfd95fed0ec28cb3c
Dhiraj salunke <dhirajraje113@gmail.com>
2023-01-06 18:41:59 +0530

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,3 @@
+export default function Button({children}:any) {
+    return <button type="button" className="relative px-6 py-2 text-white overflow-hidden font-semibold rounded bg-indigo-600">{children}</button>
+}
